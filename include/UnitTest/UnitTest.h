5a350ce571858c1825f9843ec1b0d79970376298
mousubin <mousubin@hotmail.com>
2014-09-14 23:56:07 +0800

    no message

--- include/UnitTest/UnitTest.h
+++ include/UnitTest/UnitTest.h
@@ -14,19 +14,30 @@
 namespace MUnitTest {
     
     
-#define TestCase(X) class TestCase_##X : public MTestCase
-#define TestItem(X) public: void TestItem_##X()
-    //{ reg(TestItem#X); }
+#define TestCase(X) class TestCase_##X : public MTestCase { \
+    typedef TestCase_##X TestCase_Class; \
+    void run(){ \
+        std::vector<TestItemFunc>::iterator iter;\
+        for (iter = _items.begin(); iter != _items.end(); iter++) {\
+            (this->*(*iter))();\
+        }\
+    }\
+    void _anonymous_()
+#define EndTestCase };
+#define TestItem(X) } public: void TestItem_##X() { \
+    reg(static_cast<TestItemFunc>(&TestCase_Class::TestItem_##X));
+
     
     class MTestCase
     {
     public:
-        typedef void (*TestItemFunc)(void);
+        typedef void (MTestCase::*TestItemFunc)(void);
         std::vector<TestItemFunc> _items;
     public:
         void reg(TestItemFunc func) {
             _items.push_back(func);
         }
+        
     };
     
     class MTestSuite
--- src/Test/MCoreTestApp/MCoreTestApp/T_vec3.h
+++ src/Test/MCoreTestApp/MCoreTestApp/T_vec3.h
@@ -13,33 +13,46 @@
 #include "UnitTest/UnitTest.h"
 #include "math/vec3.h"
 
-
 using namespace MCore;
 using namespace MUnitTest;
 
-TestCase(vec3) {
-    TestItem(0){
-        vec3f v1(1.0f, 2.0f, 3.0f), v2(2, 3, 4);
-        vec3d v3;
-        v3 = v1;
-        v3 = v1 + v2;
-        v3 = v1 - v2;
-        v3 = v1 * v2;
-        v3 = v1 ^ v2;
-        v3 = v1
-        v3 = v1;
-        v3.add(v2);
-        v3 = v1;
-        v3.sub(v2);
-        v3 = v1;
-        v3.multi(v2);
-        v3 = v1;
-        v3.nomalize();
-        v3 = v1;
-        
+
+TestCase(vec3)
+{
+    TestItem(0) {
+          vec3f v1(1.0f, 2.0f, 3.0f), v2(2, 3, 4);
+//        vec3d v3;
+//        v3 = v1;
+//        v3 = v1 + v2;
+//        v3 = v1 - v2;
+//        v3 = v1 * v2;
+//        v3 = v1 ^ v2;
+//        v3 = v1
+//        v3 = v1;
+//        v3.add(v2);
+//        v3 = v1;
+//        v3.sub(v2);
+//        v3 = v1;
+//        v3.multi(v2);
+//        v3 = v1;
+//        v3.nomalize();
+//        v3 = v1;
+    }
+    TestItem(1) {
     }
 }
+EndTestCase
 
+
+TestCase(vec4) {
+    TestItem(0) {
+        vec3f v1(1.0f, 2.0f, 3.0f), v2(2, 3, 4);
+
+    }
+    TestItem(1) {
+    }
+}
+EndTestCase
 
 
 #endif /* defined(__MCoreTestApp__T_vec3__) */
